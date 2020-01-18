4a31693d3a6712fb79d2eca1f552923ef5285278
h2222 <1311367609@qq.com>
2020-01-19 04:03:28 +0800

    leaning pandas

---
+++ pandas_learn/pandas_learn.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+print(pd.__version__)
+
+
+
+# 列表
+index = ['a', 'b', 'c' ,'d', 'e']
+s = pd.Series([1,2,3,4,5], index=index)
+print(s)
+
+
+# 字典创建series
+s2 = pd.Series({1:'a',2:'2'})
+print(s2)
