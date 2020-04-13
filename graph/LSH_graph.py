d4ae1e7f456b4b6f3b5cdce6dd5a12d32f73d731
unknown <jiaxiang.hao@bairong.ad.com>
2020-04-13 21:36:32 +0800

    up

--- graph/LSH_graph.py
+++ graph/LSH_graph.py
@@ -16,19 +16,39 @@ b : bands per signature matrix
 
 import numpy as np
 from matplotlib import pyplot as plt
-import matplotlib 
 
 def lsh_graph():
-    r_b = [(3, 10), (5, 20), (5, 50)]
-    s = np.arange(0.01, 1, 0.001)
+    r_b = [(3, 10, '*', 'r=3, b=10'), (5, 20, 'o', 'r=5 b=20'), (5, 50, 'x', 'r=5 b=50')]
+    s = np.arange(0.01, 1, 0.05)
 
-    for r, b in r_b:
+    plt.title('LSH probability S_curve')
+    for r, b, mark, label in r_b:
         p = 1 - (1 - s**r)**b
-        plt.title('LSH probability S_curve')
-        plt.plot(s, p)
-        plt.show()
-                
+        plt.plot(s, p, marker=mark, label=label)
+        plt.legend()
+    plt.margins(0)
+    plt.subplots_adjust(bottom=0.01)
+    plt.ylabel('probability')
+    plt.yticks([i*0.1 for i in range(0, 12, 1)])
+    plt.xticks([i*0.01 for i in range(0, 100, 10)])
+    plt.xlabel('Similarity')
+    plt.show()
+
+
+
+def pf_graph():
+    k = np.arange(1, 10, 1)
+    m = 2
+    n = 10
+    p = (1 - np.exp(- k*m/n))**k
+
+    plt.plot(k, p, marker='*')
+    plt.ylabel('probability of false-positive')
+    plt.xlabel('the number of hash functions')
+    plt.show()
+
+
 
 
 if __name__ == "__main__":
-    lsh_graph()
+    pf_graph()
